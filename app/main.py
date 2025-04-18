dbd2ef9a79c8a0ea744b6fbfea38b4d8488149ff
Sander Halvorsen <sandhal@stud.ntnu.no>
2025-04-18 13:07:56 +0200

    lagt til crud ops og account routes. updata main.py så det inkluderer routes.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from app.database import Base, engine
 from app import models
-app = FastAPI()
+from app.routers import accounts, tweets
+
+# Lager databasen og tabellene
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Twitter Clone API")
+
+# Inkluder routers
+app.include_router(accounts.router)
+app.include_router(tweets.router)
 
 @app.get("/")
 def read_root():
